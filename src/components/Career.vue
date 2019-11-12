fdbac391cfb7ad44812db255cfbfe7b143e970fa
Morteza Ziyae <m.ziaeemehr@gmail.com>
2019-11-12 11:38:53 +0330

    add career texts

--- src/components/Career.vue
+++ src/components/Career.vue
@@ -1,8 +1,24 @@
 <template>
-  <v-content>
-    <v-container fluid>
-      <div style="color: white;">career...</div>
-    </v-container>
+  <v-content class="pt-0">
+    <div>
+      <div class="full-width-banner"></div>
+      <v-container class="white--text">
+        <article>
+          <h1 class="headline mb-2">.net Programmer</h1>
+          <p class="subtitle-1 mb-4">Onsite or Remote</p>
+          <div class="body-1 grey--text text--lighten-1 mb-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad necessitatibus harum, illo molestiae temporibus alias officia dolore rerum suscipit deserunt mollitia ratione, minima veniam! Quod recusandae autem adipisci perferendis architecto distinctio debitis nobis illum temporibus dolore provident odio velit iure harum culpa esse, natus sint eaque perspiciatis, ad repellat alias dolores. Fuga excepturi facere voluptates dolores id iusto rem sunt ex dolore? Nostrum animi reiciendis cumque hic? Veniam enim recusandae aliquam obcaecati quos doloremque et officiis deserunt dolor. Harum illum veniam optio, quasi eligendi rerum autem corporis velit at quos iusto voluptate! Accusantium quos, reprehenderit doloremque corporis ipsum commodi quod!</div>
+
+          <div class="typeform-wrapper mb-4">
+            <div class="typeform-widget" data-url="https://paulchrisluke.typeform.com/to/TNDX88" data-transparency="50" style="width: 100%; height: 500px;"></div> <script type="application/javascript"> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script> <div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;"> powered by <a href="https://admin.typeform.com/signup?utm_campaign=TNDX88&utm_source=typeform.com-14889424-Free&utm_medium=typeform&utm_content=typeform-embedded-poweredbytypeform&utm_term=EN" style="color: #999" target="_blank">Typeform</a> </div>
+          </div>
+
+          <aside>
+            <h2 class="text-xs-center title">Explore Career</h2>
+          </aside>
+        </article>
+
+      </v-container>
+    </div>
   </v-content>
 </template>
 
@@ -12,5 +28,8 @@ export default Vue.extend({});
 </script>
 
 <style scoped>
-
+.full-width-banner {
+  height: 400px;
+  background: #aaa;
+}
 </style>
